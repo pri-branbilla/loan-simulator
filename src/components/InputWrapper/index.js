33249705fc9f54f995b4b6c1153f83ece334c767
Priscilla Nascimento <priscillans2712@gmail.com>
2019-04-19 18:00:25 -0300

    add InputWrapper component

---
+++ src/components/InputWrapper/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './styles.css'
+
+export class InputWrapper extends React.PureComponent {
+  render () {
+    const { label, children, inputId } = this.props
+
+    return (
+      <div className="field">
+        <label htmlFor={inputId}>{label}</label>
+        {children}
+      </div>
+    )
+  }
+}
+
+InputWrapper.propTypes = {
+  inputId: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType(
+    [
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+    ],
+  ).isRequired,
+}
